cc6349abf9a33ec46f6d6fa4152788139c90233d
Alex Guevara <a.guevara23@gmail.com>
2018-06-05 18:32:45 -0400

    initial commit

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,15 +1,60 @@
-//
-//  main.cpp
-//  BullCowGame
-//
-//  Created by Alexandre Guevara on 6/3/18.
-//  Copyright © 2018 Alex Guevara. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
+constexpr int NumberOfTurns = 5;
+
+int main() {
+    do {
+        PrintIntro();
+        PlayGame();
+        cout << endl;
+    }
+    while (AskToPlayAgain());
+    
     return 0;
 }
+
+// Introduce the game
+void PrintIntro() {
+    constexpr int WORD_LENGTH = 9;
+    cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+    cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+    return;
+}
+
+void PlayGame() {
+    for (int count = 0; count < NumberOfTurns; count++) {
+        string Guess = GetGuess();
+        // repeat the guess
+        cout << "Your guess is: " << Guess << endl;
+        cout << endl;
+    }
+}
+
+string GetGuess(){
+    // get a guess from the player
+    cout << "Enter your guess: ";
+    string Guess = "";
+    getline (cin, Guess);
+    return Guess;
+}
+
+bool AskToPlayAgain() {
+    cout << "Do you want to play again? ";
+    string Response = "";
+    getline(cin, Response);
+    char Answer = Response[0];
+    if (Answer == 'y' || Answer == 'Y') {
+        cout << "Ok, let's play again!\n";
+        return true;
+    }
+    cout << "Thank you for playing!\n";
+    return false;
+}
+
+
---
+++ BullCowGame/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  BullCowGame
+//
+//  Created by Alexandre Guevara on 6/5/18.
+//  Copyright © 2018 Alex Guevara. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
